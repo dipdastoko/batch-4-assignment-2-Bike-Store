de1c13cbee30d929426209c19b6f24c30fbbb442
rpamgi <dipdastoko@gmail.com>
2025-01-25 13:40:38 +0600

    creat-bike api created successfully

--- src/app/modules/products/products.controllers.ts
+++ src/app/modules/products/products.controllers.ts
@@ -2,13 +2,21 @@ import { Request, Response } from 'express';
 import { prouductServices } from './products.services';
 
 const createABike = async (req: Request, res: Response) => {
-  const bikeData = req.body;
-  const result = await prouductServices.createBikeIntoDB(bikeData);
-  res.send(200).json({
-    message: 'Bike created successfully',
-    success: true,
-    data: result,
-  });
+  try {
+    const bikeData = req.body;
+    const result = await prouductServices.createBikeIntoDB(bikeData);
+    res.status(200).json({
+      message: 'Bike created successfully',
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Something went wrong!',
+      success: false,
+      data: error,
+    });
+  }
 };
 
 export const BikeControllers = {
